234eca2e09543edc879019ad2ef0673bbbf612cd
DewewRHINO <jrjayme@cpp.edu>
2023-10-03 09:10:23 -0700

    Used cases instead of if statements for the total, and iterated through buttons

--- index.js
+++ index.js
@@ -1,85 +1,77 @@
-// Setting results to the element that holds the result ID. This is where we get our calculations.
-let result = document.getElementById("result");
+// Getting the element with ID "result" where the calculations will be displayed.
+let res = document.getElementById("result");
 
-// Store the buttons in an array with class name math-btn
-let buttons = Array.from(document.getElementsByClassName("math-btn"));
+// Getting all buttons with the class name "math-btn".
+let btns = Array.from(document.getElementsByClassName("math-btn"));
 
-// Resources to calculate expression (I did not know what I was doing lol)
-// Learning how to parse the expression: https://www.geeksforgeeks.org/expression-evaluation/
+// Function to calculate the expression. Honestly, I found a cool resource that helped a lot: https://www.geeksforgeeks.org/expression-evaluation/
+function calcExpr(expr) {
+    const ops = ['+', '-', '*', '/'];  // These are the operators I can handle for now.
 
-function calculateExpression(expression) {
-    // We want to split the expression when we see these operators (+, -, *, and /), and then from there do the calculations. If I had paranthesis I would most likely have to rewrite this.
-    const operators = ['+', '-', '*', '/'];
-
-    // Use a custom function to split the expression
-    function splitExpression(expr) {
-        const result = [];    // An array to store the split components
-        let current = '';     // A string to monitor the numbers being put in
+    // This function splits the expression into numbers and operators, it's kinda cool.
+    function splitExpr(e) {
+        let result = [];  
+        let current = '';  
     
-        for (const char of expr) {
-            if (operators.includes(char)) {
-                // marks the end of the current component based on the operator
-                // push the current component to the result array, push the operator itself, and reset 'current'.
-                result.push(current);
-                result.push(char);
+        for (let c of e) {
+            if (ops.includes(c)) {
+                result.push(current, c);
                 current = '';
             } else {
-                // If the character is not an operator, it belongs to the current component.
-                // So, we add it to the 'current' string.
-                current += char;
+                current += c;
             }
         }
     
         result.push(current);
-    
-        // Deletes any empty component.
-        return result.filter((item) => item !== '');
+        return result.filter((item) => item !== '');  // Getting rid of the empty stuff.
     }
     
 
-    const tokens = splitExpression(expression);
+    let tokens = splitExpr(expr);
 
-    // Convert operands to numbers
-    const operands = tokens.filter((token) => !operators.includes(token)).map((operand) => parseFloat(operand));
+    // Turning the string numbers into actual numbers.
+    let nums = tokens.filter((t) => !ops.includes(t)).map((num) => parseFloat(num));
 
-    // Calculate the result based on the operator
-    let result = operands[0];
-    for (let i = 1; i < operands.length; i++) {
-        if (operators.includes(tokens[i])) {
-            if (tokens[i] === '+') {
-                result += operands[i];
-            } else if (tokens[i] === '-') {
-                result -= operands[i];
-            } else if (tokens[i] === '*') {
-                result *= operands[i];
-            } else if (tokens[i] === '/') {
-                if (operands[i] === 0) {
-                    throw new Error("Why are you dividing by 0 SMH.");
+    // Okay, let's calculate this!
+    let total = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        switch(tokens[i]) {
+            case '+':
+                total += nums[i];
+                break;
+            case '-':
+                total -= nums[i];
+                break;
+            case '*':
+                total *= nums[i];
+                break;
+            case '/':
+                if (nums[i] === 0) {
+                    throw "You can't divide by zero, silly!";
                 }
-                result /= operands[i];
-            }
+                total /= nums[i];
+                break;
         }
     }
 
-    return result;
+    return total;
 }
 
-// Check the operations of the button and do the calculations that way.
-// We use the map function to iterate over all the buttons.
-buttons.map((button) => {
-    button.addEventListener("click", (e) => {
-        if (e.target.innerText === "C") {
-            result.innerText = "";
-        } else if (e.target.innerText === "=") {
+// Looping through all the buttons to see what they do when clicked.
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        let text = e.target.innerText;
+        
+        if (text === "C") {
+            res.innerText = "";
+        } else if (text === "=") {
             try {
-                // Here is where we try to calculate the expression, this is where I would have put the eval() function but, we are making our own.
-                result.innerText = calculateExpression(result.innerText);
+                res.innerText = calcExpr(res.innerText);
             } catch (err) {
-                result.innerText = err;
+                res.innerText = "Error: " + err;
             }
         } else {
-            result.innerText += e.target.innerText;
+            res.innerText += text;
         }
     });
 });
-
